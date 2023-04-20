01715e7360e21f388e17c85b9360dd09ae829edd
HFScripts <130812709+HFScripts@users.noreply.github.com>
2023-04-20 23:52:45 +1200

    Update bs.py

--- bs.py
+++ bs.py
@@ -4,6 +4,9 @@ import sys
 # Find the path to the Blender Python executable
 blender_python = sys.executable
 
-# Install the BeautifulSoup library using pip
+# Install required packages using pip
 subprocess.call([blender_python, "-m", "ensurepip"])
 subprocess.call([blender_python, "-m", "pip", "install", "beautifulsoup4"])
+subprocess.call([blender_python, "-m", "pip", "install", "requests"])
+subprocess.call([blender_python, "-m", "pip", "install", "Pillow"])
+subprocess.call([blender_python, "-m", "pip", "install", "urllib3"])
