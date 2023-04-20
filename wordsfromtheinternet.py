b81072be08e74ca38be3be48d7931a97bfd15723
HFScripts <130812709+HFScripts@users.noreply.github.com>
2023-04-20 23:41:41 +1200

    Update wordsfromtheinternet.py

--- wordsfromtheinternet.py
+++ wordsfromtheinternet.py
@@ -7,7 +7,15 @@ bl_info = {
 import bpy
 import requests
 from bs4 import BeautifulSoup
-
+import requests
+from bs4 import BeautifulSoup
+from io import BytesIO
+from PIL import Image
+from collections import Counter
+import itertools
+import math
+import re
+from urllib.parse import urlparse, urlunparse
 
 class TEXT_OT_title_from_url(bpy.types.Operator):
     bl_idname = "object.title_from_url"
@@ -21,59 +29,133 @@ class TEXT_OT_title_from_url(bpy.types.Operator):
     )
 
     def execute(self, context):
-        
+        # Define a function to calculate the Euclidean distance between two colors
+        def color_distance(color1, color2):
+            r1, g1, b1 = color1
+            r2, g2, b2 = color2
+            return math.sqrt((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2)
+    
+        # Get the website URL from user input
+        url = self.url
+    
+        # Your provided code to get the most_common_colors
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+    
+        favicon_url = None
+        for tag in soup.find_all('link'):
+            if 'icon' in tag.get('rel') or 'shortcut icon' in tag.get('rel'):
+                favicon_url = tag.get('href')
+                break
+    
+        if not favicon_url:
+            favicon_url = url.rstrip('/') + '/favicon.ico'
+    
+        try:
+            favicon_response = requests.get(favicon_url)
+            favicon_data = BytesIO(favicon_response.content)
+            favicon_image = Image.open(favicon_data)
+    
+        except requests.exceptions.RequestException:
+            parsed_url = urlparse(url)
+            path = favicon_url
+            if path.startswith('/'):
+                path = path[1:]
+            new_favicon_url = urlunparse((parsed_url.scheme, parsed_url.netloc, path, '', '', ''))
+            try:
+                favicon_response = requests.get(new_favicon_url)
+                favicon_data = BytesIO(favicon_response.content)
+                favicon_image = Image.open(favicon_data)
+                favicon_url = new_favicon_url
+            except requests.exceptions.RequestException:
+                self.report({'ERROR'}, "Could not retrieve the favicon from " + favicon_url)
+                return {'CANCELLED'}
+    
+        colors = favicon_image.getdata()
+        colors = [(color[0], color[1], color[2]) for color in colors]
+        unique_colors = list(set(colors))
+    
+        color_counts = Counter(colors)
+    
+        close_color_pairs = set()
+        for color1, color2 in itertools.combinations(unique_colors, 2):
+            if color_distance(color1, color2) < 50:
+                close_color_pairs.add(frozenset([color1, color2]))
+    
+        most_common_colors = []
+        for color, count in color_counts.most_common():
+            if not any(frozenset([color, common_color]) in close_color_pairs for common_color in most_common_colors):
+                most_common_colors.append(color)
+                if len(most_common_colors) == 3:
+                    break
+    
         # Retrieve the HTML content of the website
         response = requests.get(self.url)
         html_content = response.content
-            
+    
         # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(html_content, 'html.parser')
-            
+    
         # Extract the title of the website from the HTML if it exists
         if soup.title is not None:
             title = soup.title.string
-            
+    
+            # Split the title into words
+            words = re.findall(r'\w+', title)
+    
             # Create a new Blender scene
             bpy.ops.scene.new(type='NEW')
-            
-            # Create a text object
-            bpy.ops.object.text_add()
-            obj = bpy.context.object
-            
-            # Set the text of the object to the website title
-            obj.data.body = title
-            
-            # Add thickness to the text object by adjusting the extrude property
-            obj.data.extrude = 0.2
-            
-            # Rotate the text object around the X-axis by 90 degrees (1.5708 radians)
-            obj.rotation_euler[0] = 1.5708
-            
-            # Add some bevel and extrusion to give the text some depth
-            bpy.ops.object.modifier_add(type='BEVEL')
-            bpy.ops.object.modifier_add(type='SOLIDIFY')
-            
-            # Convert the text object to a mesh
-            bpy.ops.object.convert(target='MESH')
     
-            # Add a displace modifier
-            bpy.ops.object.modifier_add(type='DISPLACE')
-            displace_modifier = obj.modifiers['Displace']
-            displace_modifier.direction = 'NORMAL'
-            displace_modifier.strength = 0
-            displace_modifier.mid_level = 0.5
+            for i, word in enumerate(words):
+                # Create a text object for each word
+                bpy.ops.object.text_add()
+                obj = bpy.context.object
             
-            # Create a texture for the displace modifier
-            displace_texture = bpy.data.textures.new(name="DisplaceTexture", type='CLOUDS')
-            displace_texture.noise_scale = 0.5
-            displace_modifier.texture = displace_texture
+                # Set the location of the object along the X-axis to add space between words
+                obj.location.x = 4 * i # Adjust the multiplier (2.5 here) to increase or decrease the spacing between words
             
-            # Add keyframes for the strength property
-            obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=1)
-            obj.modifiers['Displace'].strength = 1.0
-            obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=50)
-            obj.modifiers['Displace'].strength = 0.0
-            obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=100)
+                # Set the text of the object to the current word
+                obj.data.body = word            
+                # Apply color from most_common_colors to every second word
+                if i % 2 == 1 and most_common_colors:
+                    color_index = (i // 2) % len(most_common_colors)
+                    color = [c / 255 for c in most_common_colors[color_index]]
+                    material = bpy.data.materials.new(name="WordColor")
+                    material.use_nodes = False
+                    material.diffuse_color = color + [1]
+                    obj.data.materials.append(material)
+            
+                # Add thickness to the text object by adjusting the extrude property
+                obj.data.extrude = 0.2
+            
+                # Rotate the text object around the X-axis by 90 degrees (1.5708 radians)
+                obj.rotation_euler[0] = 1.5708
+            
+                # Add some bevel and extrusion to give the text some depth
+                bpy.ops.object.modifier_add(type='BEVEL')
+                bpy.ops.object.modifier_add(type='SOLIDIFY')
+            
+                # Convert the text object to a mesh
+                bpy.ops.object.convert(target='MESH')
+            
+                # Add a displace modifier
+                bpy.ops.object.modifier_add(type='DISPLACE')
+                displace_modifier = obj.modifiers['Displace']
+                displace_modifier.direction = 'NORMAL'
+                displace_modifier.strength = 0
+                displace_modifier.mid_level = 0.5
+            
+                # Create a texture for the displace modifier
+                displace_texture = bpy.data.textures.new(name="DisplaceTexture", type='CLOUDS')
+                displace_texture.noise_scale = 0.5
+                displace_modifier.texture = displace_texture
+            
+                # Add keyframes for the strength property
+                obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=1)
+                obj.modifiers['Displace'].strength = 1.0
+                obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=50)
+                obj.modifiers['Displace'].strength = 0.0
+                obj.keyframe_insert(data_path="modifiers[\"Displace\"].strength", frame=100)
 
             
             return {'FINISHED'}
